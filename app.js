4e4a468bf7fc9e55ea5b509f1bf5c7336f4a5579
刘春洋 <lcya86@qq.com>
2013-09-25 17:30:06 +0800

    初步架构
    搭好了初步的架构，实现基于session-mongoose的登陆功能

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -9,29 +8,77 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var ejs = require('ejs');
-
+var SessionStore = require("session-mongoose")(express);
 var app = express();
+
+//session store
+var store = new SessionStore({
+	url: "mongodb://localhost/session",
+	interval: 120000
+})
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.engine('.html',ejs.__express);
+app.engine('.html', ejs.__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.cookieSession({
+	secret: 'fens.me'
+}));
+app.use(express.session({
+	secret: 'fens.me',
+	store: store,
+	cookie: {
+		maxAge: 900000
+	}
+}));
+app.use(function(req, res, next) {
+	res.locals.user = req.session.user;
+	var err = req.session.error;
+	delete req.session.error;
+	res.locals.message = '';
+	if (err) res.locals.message = '<div class="alert alert-error">' + err + '</div>';
+	next();
+});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
+}
+
+//authentication&notAuthentication
+function authentication(req,res,next){
+	if(!req.session.user){
+		req.session.error='请先登陆';
+		return res.redirect('/login');
+	}
+	next();
+}
+
+function notAuthentication(req,res,next){
+	if(req.session.user){
+		req.session.error='已登陆';
+		return res.redirect('/');
+	}
+	next();
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.all('/login',notAuthentication);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout',authentication);
+app.get('/logout', routes.logout);
+app.get('/home',authentication);
+app.get('/home', routes.home);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
