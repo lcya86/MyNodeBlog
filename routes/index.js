818d468f1a542a23fc53fb10fd1647043b3f39ab
lcya86 <lcya86@qq.com>
2014-01-02 22:05:23 +0800

    更改文章排序

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-
 /*
  * GET home page.
  */
@@ -7,153 +6,210 @@ var app = require('../app');
 var model = require('../models/models');
 
 // enter index page
-exports.index = function(req, res){
-	model.Visitor.findOne({ip:req.ip},function(err,visitor){
-		if(err){
+exports.index = function(req, res) {
+	model.Visitor.findOne({
+		ip: req.ip
+	}, function(err, visitor) {
+		if (err) {
 			console.error(err);
 		}
-		if(visitor){
+		if (visitor) {
 			visitor.frequency++;
-			visitor.lastVisitTime=Date.now();
+			visitor.lastVisitTime = Date.now();
 			visitor.save();
-		}else{
-			model.Visitor.create({ip:req.ip,frequency:1,lastVisitTime:Date.now()},function(err){
-				if(err) console.error(err);
+		} else {
+			model.Visitor.create({
+				ip: req.ip,
+				frequency: 1,
+				lastVisitTime: Date.now()
+			}, function(err) {
+				if (err) console.error(err);
 				console.log('create visitor Ok');
 			});
 		}
 	});
-	model.Post.find(function(err,posts){
-	  return res.render('index', {title: 'L\'s blog',posts:posts,user:req.session.user});
-	})
+	model.Post.find().sort('-update -like').exec(function(err, posts) {
+		return res.render('index', {
+			title: 'L\'s blog',
+			posts: posts,
+			user: req.session.user
+		});
+	});
 };
 
-exports.login = function(req,res){
-	res.render('login',{title:'用户登陆'});
+exports.login = function(req, res) {
+	res.render('login', {
+		title: '用户登陆'
+	});
 }
 
-exports.doLogin = function(req,res){
+exports.doLogin = function(req, res) {
 	var user = {
-		username:'admin',
-		password:'admin'
+		username: 'admin',
+		password: 'admin'
 	}
-	if(req.body.username===user.username && req.body.password===user.password){
-		req.session.user=user;
+	if (req.body.username === user.username && req.body.password === user.password) {
+		req.session.user = user;
 		return res.redirect('/home');
-	}else{
-		req.session.error='用户名或密码不正确';
+	} else {
+		req.session.error = '用户名或密码不正确';
 		return res.redirect('/login');
 	}
 }
 
-exports.logout = function(req,res){
-	req.session.user=null;
+exports.logout = function(req, res) {
+	req.session.user = null;
 	return res.redirect('/');
 }
 
-exports.home = function(req,res){
+exports.home = function(req, res) {
 	var user = {
-		username:'admin',
-		password:'admin'
+		username: 'admin',
+		password: 'admin'
 	}
-	app.io.sockets.on('connection',function(socket){
-		socket.emit('user',{ip:req.ip,cookies:req.cookies,body:req.body});
+	app.io.sockets.on('connection', function(socket) {
+		socket.emit('user', {
+			ip: req.ip,
+			cookies: req.cookies,
+			body: req.body
+		});
 	});
-	model.Visitor.find(function(err,visitors){
-		if(err){
+	model.Visitor.find(function(err, visitors) {
+		if (err) {
 			console.error(err);
 		}
-		return res.render('home',{title:'home',visitors:visitors});
+		return res.render('home', {
+			title: 'home',
+			visitors: visitors
+		});
 	});
 }
 
-exports.post = function(req,res){
-	return res.render('post',{title:'写文章',post:null});
+exports.post = function(req, res) {
+	return res.render('post', {
+		title: '写文章',
+		post: null
+	});
 }
 
-exports.doPost = function(req,res){
+exports.doPost = function(req, res) {
 	console.log('doPost');
 	var title = req.body.write.match(/^.+$/m);
-	model.Post.create({body:req.body.write,title:title[0],tags:[]},function(err){
-		if(err) console.error(err);
+	model.Post.create({
+		body: req.body.write,
+		title: title[0],
+		tags: []
+	}, function(err) {
+		if (err) console.error(err);
 		console.log('createOk');
 	});
 	return res.redirect('/');
 }
 
-exports.getPost = function(req,res){
+exports.getPost = function(req, res) {
 	var title = req.params.title;
-	if(title){
-		model.Post.find({title:title},function(err,post){
-			if(err){
+	if (title) {
+		model.Post.find({
+			title: title
+		}, function(err, post) {
+			if (err) {
 				console.error(err);
-				return res.render('404',{title:'404'});
+				return res.render('404', {
+					title: '404'
+				});
 			}
-			return res.render('getpost',{title:title,post:post});
+			return res.render('getpost', {
+				title: title,
+				post: post
+			});
 		});
-	}else{
-		return res.render('404',{title:'404'});
+	} else {
+		return res.render('404', {
+			title: '404'
+		});
 	}
 }
 
-exports.edit = function(req,res){
+exports.edit = function(req, res) {
 	var title = req.params.title;
-	if(title){
-		model.Post.findOne({title:title},function(err,post){
-			if(err){
+	if (title) {
+		model.Post.findOne({
+			title: title
+		}, function(err, post) {
+			if (err) {
 				console.error(err);
-				return res.render('404',{title:'404'});
+				return res.render('404', {
+					title: '404'
+				});
 			}
-			return res.render('post',{title:title,post:post});
+			return res.render('post', {
+				title: title,
+				post: post
+			});
 		});
-	}else{
-		return res.render('404',{title:'404'});
+	} else {
+		return res.render('404', {
+			title: '404'
+		});
 	}
 }
 
-exports.doEdit = function(req,res){
+exports.doEdit = function(req, res) {
 	var title = req.params.title;
-	if(title){
-		model.Post.findOneAndUpdate({title:title},{body:req.body.write},function(err){
-			if(err){
+	if (title) {
+		model.Post.findOneAndUpdate({
+			title: title
+		}, {
+			body: req.body.write,
+			update: Date.now()
+		}, function(err) {
+			if (err) {
 				console.error(err);
-				return res.render('404',{title:'404'});
+				return res.render('404', {
+					title: '404'
+				});
 			}
 			return res.redirect('/');
 		});
-	}else{
-		return res.render('404',{title:'404'});
+	} else {
+		return res.render('404', {
+			title: '404'
+		});
 	}
 }
 
-exports.doRemove = function(req,res){
+exports.doRemove = function(req, res) {
 	var title = req.params.title;
-	if(title){
-		model.Post.findOneAndRemove({title:title},function(err){
-			if(err){
+	if (title) {
+		model.Post.findOneAndRemove({
+			title: title
+		}, function(err) {
+			if (err) {
 				console.error(err);
 			}
 			return res.redirect('/');
 		});
-	}else{
+	} else {
 		return res.redirect('/');
 	}
 }
 
-exports.doLike = function(req,res){
+exports.doLike = function(req, res) {
 	var title = req.params.title;
-	if(title){
-		model.Post.findOne({title:title},function(err,post){
-			if(err){
+	if (title) {
+		model.Post.findOne({
+			title: title
+		}, function(err, post) {
+			if (err) {
 				console.error(err);
 			}
-			if(post){
+			if (post) {
 				post.like++;
 				post.save();
 			}
 			return res.redirect('/');
 		});
-	}else{
+	} else {
 		return res.redirect('/');
 	}
 }
