827c1a7223836f0b8d4f77512a130f51898db383
lcya86 <lcya86@qq.com>
2014-03-15 17:51:22 +0800

    improve the function of visitor showing

--- routes/index.js
+++ routes/index.js
@@ -8,40 +8,42 @@ var model = require('../models/models');
 exports.index = function(req, res) {
 	var ip = req.ip;
 	var agent = req.headers['user-agent'];
-	console.log(ip+" : "+agent);
-	model.Visitor.findOne({
-		ip: ip
-	}, function(err, visitor) {
-		if (err) {
-			console.error(err);
-		}
-		if (visitor) {
-			visitor.frequency++;
-			visitor.lastVisitTime = Date.now();
-			visitor.save();
-		} else {
-			require('http').get('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip='+ip,function(response){
-				response.setEncoding('utf-8');
-				response.on('data',function(chunk){
-					var ip_info = JSON.parse(chunk);
-					model.Visitor.create({
-						ip: ip,
-						frequency: 1,
-						agent:agent,
-						country:ip_info.country,
-						province:ip_info.province,
-						city:ip_info.city,
-						lastVisitTime: Date.now()
-					}, function(err) {
-						if (err) console.error(err);
-						console.log('create visitor Ok');
+	console.log(ip + " : " + agent);
+	if (agent && !/bingbot|Baiduspider|Googlebot/.test(agent)) {
+		model.Visitor.findOne({
+			ip: ip
+		}, function(err, visitor) {
+			if (err) {
+				console.error(err);
+			}
+			if (visitor) {
+				visitor.frequency++;
+				visitor.lastVisitTime = Date.now();
+				visitor.save();
+			} else {
+				require('http').get('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=' + ip, function(response) {
+					response.setEncoding('utf-8');
+					response.on('data', function(chunk) {
+						var ip_info = JSON.parse(chunk);
+						model.Visitor.create({
+							ip: ip,
+							frequency: 1,
+							agent: agent,
+							country: ip_info.country,
+							province: ip_info.province,
+							city: ip_info.city,
+							lastVisitTime: Date.now()
+						}, function(err) {
+							if (err) console.error(err);
+							console.log('create visitor Ok');
+						});
 					});
+				}).on('error', function(err) {
+					console.log("Got error:" + err.message);
 				});
-			}).on('error',function(err){
-				console.log("Got error:"+err.message);
-			});
-		}
-	});
+			}
+		});
+	}
 	model.Post.find().sort('-update -like').exec(function(err, posts) {
 		return res.render('index', {
 			title: '大L的博客',
