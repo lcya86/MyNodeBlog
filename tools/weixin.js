609c736918d3f38d67572d34a1ab0d3053a69b17
刘春洋 <lcya86@qq.com>
2014-05-02 23:37:17 +0800

    Update weixin.js

--- tools/weixin.js
+++ tools/weixin.js
@@ -1,14 +1,12 @@
 var crypto = require('crypto');
-var shasum = crypto.createHash('sha1');
 var TOKEN = 'lcya86'
 exports.checkSignature = function(signature,timestamp,nonce){
 	var tmpArr = [TOKEN,timestamp,nonce];
 	tmpArr.sort();
 	tmpStr = tmpArr.join('');
-	console.log(tmpStr);
+	var shasum = crypto.createHash('sha1');
 	shasum.update(tmpStr);
 	tmpStr = shasum.digest('hex');
-	console.log(tmpStr);
 	if(tmpStr == signature){
 		return true;
 	}else{
