5e7653707ad68f400b551b60869a31a39d497cd4
lcya86 <lcya86@qq.com>
2014-05-03 09:30:46 +0800

    update weixin.js

--- routes/weixin.js
+++ routes/weixin.js
@@ -3,7 +3,7 @@ var weixin = require('../tools/weixin');
 exports.index = function(req, res) {
 	if (weixin.checkSignature(req.query.signature, req.query.timestamp, req.query.nonce)) {
 		var result = '';
-		console.log(req);
+		console.log(req.read);
 		return res.send('');
 	} else {
 		return res.send(404);
