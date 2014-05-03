656d7ef607796d964233124ffb2ca51b0b67a62b
lcya86 <lcya86@qq.com>
2014-05-04 07:47:45 +0800

    update xmlBodyParser

--- middleware/xmlBodyParser.js
+++ middleware/xmlBodyParser.js
@@ -1,32 +1,36 @@
 var xml2js = require('xml2js');
- 
+
 exports.xmlBodyParser = function(req, res, next) {
     if (req._body) return next();
     req.body = req.body || {};
- 
+
     // ignore GET
     if ('GET' == req.method || 'HEAD' == req.method) return next();
- 
+
     // check Content-Type
+    console.log(req.get('content-type'));
     if ('text/xml' != req.get('content-type')) return next();
- 
+
     // flag as parsed
     req._body = true;
- 
+
     // parse
     var buf = '';
     req.setEncoding('utf8');
-    req.on('data', function(chunk){ buf += chunk });
-    req.on('end', function(){  
-  		var parseString = xml2js.parseString;
-      parseString(buf, function(err, json) {
-        if (err) {
-            err.status = 400;
-            next(err);
-        } else {
-            req.body = json;
-            next();
-        }
-      });
+    req.on('data', function(chunk) {
+        buf += chunk;
+    });
+    req.on('end', function() {
+        var parseString = xml2js.parseString;
+        parseString(buf, function(err, json) {
+            if (err) {
+                err.status = 400;
+                next(err);
+            } else {
+                req.body = json;
+                console(json);
+                next();
+            }
+        });
     });
 };
