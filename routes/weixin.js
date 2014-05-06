ce7a46d7a783cd47306572017949a2efac47e55e
刘春洋 <lcya86@qq.com>
2014-05-06 09:17:00 +0800

    update weixin

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,10 +1,28 @@
 var weixin = require('../tools/weixin');
-
+var XMLWriter = require('xml-writer');
 exports.index = function(req, res) {
 	if (weixin.checkSignature(req.query.signature, req.query.timestamp, req.query.nonce)) {
 		console.log(req.body);
-		return res.send('');
+		route(req,res);
 	} else {
 		return res.send(404);
 	}
+}
+
+function route(req,res){
+	if(req.body.xml.MsgType[0]=='event'){
+		if(req.body.xml.Event[0]=='subscribe'){
+			var reply = new XMLWriter;
+			reply.startDocument();
+			reply.startElement('ToUserName').text(req.body.xml.FromUserName[0]);
+			reply.startElement('FromUserName').text(req.body.xml.ToUserName[0]);
+			var date = new Date();
+			reply.startElement('CreateTime').text(date.getTime());
+			reply.startElement('MsgType').text('text');
+			reply.startElement('Content').text('欢迎关注我的小玩意儿，这是我的博客http://lcy-blog.com欢迎来踩^_^');
+			reply.endDocument();
+			return res.send(reply.toString());
+		}
+	}
+	return res.send('');
 }
