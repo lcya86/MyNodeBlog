f026b27256bbcff78861aa97bf7bb0c454e30fb3
刘春洋 <lcya86@qq.com>
2014-05-06 09:52:02 +0800

    update weixin

--- routes/weixin.js
+++ routes/weixin.js
@@ -13,13 +13,13 @@ function route(req,res){
 	if(req.body.xml.MsgType[0]=='event'){
 		if(req.body.xml.Event[0]=='subscribe'){
 			var reply = new XMLWriter;
-			reply.startDocument();
-			reply.startElement('ToUserName').text(req.body.xml.FromUserName[0]).endElement();
-			reply.startElement('FromUserName').text(req.body.xml.ToUserName[0]).endElement();
 			var date = new Date();
+			reply.startDocument();
+			reply.writeCData('ToUserName',req.body.xml.FromUserName[0]);
+			reply.writeCData('FromUserName',req.body.xml.ToUserName[0]);
 			reply.startElement('CreateTime').text(date.getTime()+'').endElement();
-			reply.startElement('MsgType').text('text').endElement();
-			reply.startElement('Content').text('欢迎关注我的小玩意儿，这是我的博客http://lcy-blog.com欢迎来踩^_^').endElement();
+			reply.writeCData('MsgType','text');
+			reply.writeCData('Content','欢迎关注我的小玩意儿，这是我的博客http://lcy-blog.com欢迎来踩^_^');
 			reply.endDocument();
 			console.log(reply.toString());
 			return res.send(reply.toString());
