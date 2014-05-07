dc0284b8f164127e6be1e97187ea65f9e5a3c64e
lcya86 <lcya86@qq.com>
2014-05-07 21:22:03 +0800

    add test

--- routes/weixin.js
+++ routes/weixin.js
@@ -16,7 +16,7 @@ exports.test = function(req,res){
 function route(req,res){
 	if(req.body.xml.MsgType[0]=='event'){
 		if(req.body.xml.Event[0]=='subscribe'){
-			return replyText(req,res,'hi～我是刘春洋，“小玩意儿”是我平时摆忙的地方，我会不定时地做一些好玩儿的小玩意儿与大家分享，回复test可以看到我正在捣鼓神马。ps：&lt;a href=&quot;http://lcy-blog.com&quot;&gt;这是我的部落格，欢迎来访～&lt;/a&gt;');
+			return replyText(req,res,'hi～我是刘春洋，“小玩意儿”是我平时摆忙的地方，我会不定时地做一些好玩儿的小玩意儿与大家分享，回复test可以看到我正在捣鼓神马。ps：<a href="http://lcy-blog.com">这是我的部落格，欢迎来访～</a>');
 		}
 	}else if(req.body.xml.MsgType[0]=='text'){
 		if(req.body.xml.Content[0]=='test'){
