27b6422a3318197737a6c22da4401973a8f7d316
lcya86 <lcya86@qq.com>
2014-05-25 09:24:07 +0800

    weixin login

--- tools/weixin.js
+++ tools/weixin.js
@@ -37,7 +37,23 @@ exports.login = function(fn){
 				cookie += rs.replace(/Path=\//g, '');
 			}
 			console.log('cookie:'+cookie);
-			console.log('res:'+JSON.stringify(res));
+			console.log('res:'+JSON.stringify(res,censor(res)));
 			fn(null,cookie);
 		});
+}
+
+function censor(censor) {
+  var i = 0;
+
+  return function(key, value) {
+    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
+      return '[Circular]'; 
+
+    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
+      return '[Unknown]';
+
+    ++i; // so we know we aren't using the original object anymore
+
+    return value;  
+  }
 }
