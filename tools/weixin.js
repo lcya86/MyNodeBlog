5cd21e238e4521f7d5dd3f791e5b5bfb9c5076df
lcya86 <lcya86@qq.com>
2014-05-25 10:00:18 +0800

    weixin login

--- tools/weixin.js
+++ tools/weixin.js
@@ -36,6 +36,7 @@ exports.login = function(fn){
 			for(rs in res.header['set-cookie']){
 				cookie += rs.replace(/Path=\//g, '');
 			}
+			console.log('redirect:'+JSON.stringify(res.redirect));
 			console.log('base_resp:'+JSON.stringify(res.base_resp));
 			console.log('redirect_url:'+res.redirect_url);
 			fn(null,cookie);
