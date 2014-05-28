2c08d69c987d8d045298022530de37eecf4cb5c5
lcya86 <lcya86@qq.com>
2014-05-29 06:17:18 +0800

    update weixin

--- tools/weixin.js
+++ tools/weixin.js
@@ -94,7 +94,7 @@ exports.getFirstMsg = function(option,fn){
 		});
 		res.on('end',function(){
 			var msg = result.match(/wx\.cgiData[.\n]+\}\r\n/);
-			fn(msg);
+			fn(result);
 		})
 	});
 	req.end();
