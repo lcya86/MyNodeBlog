9be16c149fe70370a59addf39d20bfb0a8554a0d
lcya86 <lcya86@qq.com>
2014-05-29 21:51:59 +0800

    update weixin

--- routes/weixin.js
+++ routes/weixin.js
@@ -20,6 +20,25 @@ function route(req,res){
 			return replyText(req,res,'hi～我是刘春洋，“小玩意儿”是我平时瞎折腾的地方，我会不定时地做一些好玩儿的小玩意儿与大家分享\n回复“捣鼓啥呢”可以看到我正在捣鼓神马。\n回复"灵魂附体吧！小强！！"可以召唤灵魂和你聊天。\nps：<a href="http://lcy-blog.com">这是我的部落格，欢迎来访～</a>');
 		}
 	}else if(xml.MsgType[0]=='text'){
+		if(xml.Content[0]=='清空灵魂'){
+			for(var j = 0;j<chatList.length;j++){
+				for(prop in chatList[j]){
+					toFakeId = chatList[j][prop];
+					weixin.login(function(token,cookie){
+						var options = {
+							cookie:cookie,
+							msg:'灵魂已飘远，附体结束～',
+							token:token,
+							fakeid:toFakeId
+						}
+						weixin.sender(options,function(text){
+							console.log(text);
+						});
+					});
+				}
+			}
+			chatList = [];
+		}
 		for(var i = 0;i<chatList.length;i++){
 			if(chatList[i].hasOwnProperty(xml.FromUserName[0])){
 				inChatList = true;
