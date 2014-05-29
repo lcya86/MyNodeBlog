0c21c78e22e705ab36ec6060c6fd189cbb1ef73e
刘春洋 <lcya86@qq.com>
2014-05-29 14:04:53 +0800

    update weixin

--- tools/weixin.js
+++ tools/weixin.js
@@ -93,7 +93,7 @@ exports.getFirstMsg = function(option,fn){
 			}
 		});
 		res.on('end',function(){
-			var msg = result.match(/wx\.cgiData(.|\n)+/m);
+			var msg = result.match(/wx\.cgiData(.|$)+/m);
 			fn(result);
 			console.log(msg)
 		})
