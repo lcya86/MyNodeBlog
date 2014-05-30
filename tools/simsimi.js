458aa9b5468750acb259d0b7aac2b05af2091f84
lcya86 <lcya86@qq.com>
2014-05-31 02:18:03 +0800

    update weixin

--- tools/simsimi.js
+++ tools/simsimi.js
@@ -1,33 +1,25 @@
 var http = require('http');
+http.globalAgent = 10;
 var cookie = '';
-(function(){
+(function getCookie(){
 	http.get('http://www.simsimi.com/talk.htm',function(res){
 		for(rs in res.headers['set-cookie']){
 			cookie += res.headers['set-cookie'][rs].replace(/\sPath=\/;\sHttpOnly/,'');
 		}
-		console.log(cookie);
-	}).on('error',function(e){
-		console.log('error2:'+e.message);
-	}).setTimeout(3000,function(){
-		this.destroy();
-	});
-}());
-exports.getReply = function(msg,fn){
-	
 		var headers = {
 			'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 			'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
-			'Cookie':cookie+'simsimi_uid=56208236;',
+			'Cookie':cookie,
 			'Host':'www.simsimi.com',
-			'Referer': 'http://www.simsimi.com/talk.htm',
 			'User-Agent':	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
 		}
 		var options = {
 			hostname:'www.simsimi.com',
-			path:'/func/reqN?req='+msg+'&lc=ch',
+			path:'/func/register',
+			method:'GET',
 			headers:headers
 		}
-		http.get(options,function(res){
+		http.request(options,function(res){
 			var result = '';
 			res.setEncoding('utf8');
 			res.on('data',function(chunk){
@@ -38,12 +30,50 @@ exports.getReply = function(msg,fn){
 			});
 			res.on('end',function(){
 				console.log(result);
-				fn(JSON.parse(result).sentence_resp);
+				cookie += 'simsimi_uid='+JSON.parse(result).uid+';';
+				console.log(cookie);
 			});
 		}).on('error',function(e){
 			console.log('error1:'+e.message);
-		}).setTimeout(3000,function(){
-			this.destroy();
+		}).end();
+		console.log(cookie);
+	}).on('error',function(e){
+		console.log('error2:'+e.message);
+	});
+}());
+var getReply = function(msg,fn){
+	
+	var headers = {
+		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
+		'Cookie':cookie,
+		'Host':'www.simsimi.com',
+		'Referer': 'http://www.simsimi.com/talk.htm',
+		'User-Agent':	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
+	}
+	var options = {
+		hostname:'www.simsimi.com',
+		path:'/func/reqN?req='+msg+'&lc=ch',
+		headers:headers
+	}
+	http.get(options,function(res){
+		var result = '';
+		res.setEncoding('utf8');
+		res.on('data',function(chunk){
+			result += chunk;
 		});
+		res.on('error',function(e){
+			console.log(e.message);
+		});
+		res.on('end',function(){
+			console.log(result);
+			fn(JSON.parse(result).sentence_resp);
+		});
+	}).on('error',function(e){
+		console.log('error3:'+e.message);
+	}).setTimeout(2000,function(){
+		this.destroy();
+	});
 	
 }
+
