97b23577b231bca556a833588b811afe02b7744b
lcya86 <lcya86@qq.com>
2014-05-31 01:39:44 +0800

    add simsimi

--- tools/simsimi.js
+++ tools/simsimi.js
@@ -1,10 +1,19 @@
 var http = require('http');
-exports.getReply = function(msg,fn){
+var cookie = '';
+(function(){
 	http.get('http://www.simsimi.com/talk.htm',function(res){
-		var cookie = '';
 		for(rs in res.headers['set-cookie']){
 			cookie += res.headers['set-cookie'][rs].replace(/\sPath=\/;\sHttpOnly/,'');
 		}
+		console.log(cookie);
+	}).on('error',function(e){
+		console.log('error2:'+e.message);
+	}).setTimeout(3000,function(){
+		this.destroy();
+	});
+}());
+exports.getReply = function(msg,fn){
+	
 		var headers = {
 			'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 			'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2',
@@ -36,9 +45,5 @@ exports.getReply = function(msg,fn){
 		}).setTimeout(3000,function(){
 			this.destroy();
 		});
-	}).on('error',function(e){
-		console.log('error2:'+e.message);
-	}).setTimeout(3000,function(){
-		this.destroy();
-	});
+	
 }
