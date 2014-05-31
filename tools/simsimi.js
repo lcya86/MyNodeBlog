fb1a8beefa47d1e32a10a212a78e5fe86b5d3293
lcya86 <lcya86@qq.com>
2014-05-31 09:34:55 +0800

    update weixin

--- tools/simsimi.js
+++ tools/simsimi.js
@@ -52,8 +52,8 @@ exports.getReply = function(msg,fn){
 		'Cookie':cookie,
 		'Host':'www.simsimi.com',
 		'Connection':'keep-alive',
-		'Referer': 'http://www.simsimi.com/talk.htm'
-		//'User-Agent':	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
+		'Referer': 'http://www.simsimi.com/talk.htm',
+		'User-Agent':	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
 	}
 	var options = {
 		hostname:'www.simsimi.com',
