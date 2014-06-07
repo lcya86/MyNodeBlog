0d1d027380b4f4b41835e1cf5f6f48ce8137c6f3
lcya86 <lcya86@qq.com>
2014-06-07 21:51:13 +0800

    update weixin

--- tools/weixin.js
+++ tools/weixin.js
@@ -80,8 +80,8 @@ exports.getFirstFakeId = function(option,fn){
 		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 		//'Accept-Encoding':'gzip, deflate',
 		'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
-		'Cookie':option.cookie
-		//'User-Agent':	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
+		'Cookie':option.cookie,
+		'User-Agent':	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
 	}
 
 	var options = {
