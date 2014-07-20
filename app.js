bb9fb1cee8d5ec2f3b5ef4aebaaeaf2e615cd310
刘春洋 <lcya86@qq.com>
2014-07-20 08:28:47 +0800

    add Psychological Experiment

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var xmlBodyParser = require('./middleware/xmlBodyParser');
 
 //session store
 var store = new SessionStore({
-	url: "mongodb://localhost/session",
-	interval: 120000
+    url: "mongodb://localhost/session",
+    interval: 120000
 })
 
 // all environments
@@ -34,75 +34,74 @@ app.use(xmlBodyParser.xmlBodyParser);
 app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.cookieSession({
-	secret: 'fens.me'
+    secret: 'fens.me'
 }));
 app.use(express.session({
-	secret: 'fens.me',
-	store: store,
-	cookie: {
-		maxAge: 900000
-	}
+    secret: 'fens.me',
+    store: store,
+    cookie: {
+        maxAge: 900000
+    }
 }));
 app.use(function(req, res, next) {
-	res.locals.user = req.session.user;
-	var err = req.session.error;
-	delete req.session.error;
-	res.locals.message = '';
-	if (err) res.locals.message = '<div class="alert alert-error">' + err + '</div>';
-	next();
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    delete req.session.error;
+    res.locals.message = '';
+    if (err) res.locals.message = '<div class="alert alert-error">' + err + '</div>';
+    next();
 });
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 //authentication&notAuthentication
-function authentication(req,res,next){
-	if(!req.session.user){
-		req.session.error='请先登陆';
-		return res.redirect('/login');
-	}
-	next();
+function authentication(req, res, next) {
+    if (!req.session.user) {
+        req.session.error = '请先登陆';
+        return res.redirect('/login');
+    }
+    next();
 }
 
-function notAuthentication(req,res,next){
-	if(req.session.user){
-		req.session.error='已登陆';
-		return res.redirect('/');
-	}
-	next();
+function notAuthentication(req, res, next) {
+    if (req.session.user) {
+        req.session.error = '已登陆';
+        return res.redirect('/');
+    }
+    next();
 }
 
 app.get('/', routes.Home.index);
-app.post('/',routes.Weixin.index);
-app.get('/project/doing',routes.Project.doing);
-app.get('/project/dontTouchWhite',routes.Project.dontTouchWhite);
-app.get('/project/articlesClassify',routes.Project.articlesClassify);
+app.post('/', routes.Weixin.index);
+app.get('/project/doing', routes.Project.doing);
+app.get('/project/dontTouchWhite', routes.Project.dontTouchWhite);
+app.get('/project/articlesClassify', routes.Project.articlesClassify);
+app.get('/project/PsychologicalExperiment', routes.Project.PsychologicalExperiment);
 app.get('/getpost/:id', routes.Post.getPost);
-app.get('/like/:id',routes.Post.doLike);
-app.all('/login',notAuthentication);
+app.get('/like/:id', routes.Post.doLike);
+app.all('/login', notAuthentication);
 app.get('/login', routes.Login.login);
 app.post('/login', routes.Login.doLogin);
-app.get('/logout',authentication);
+app.get('/logout', authentication);
 app.get('/logout', routes.Login.logout);
-app.get('/home',authentication);
+app.get('/home', authentication);
 app.get('/home', routes.Home.home);
-app.get('/post',authentication);
+app.get('/post', authentication);
 app.get('/post', routes.Post.post);
-app.post('/post',authentication);
+app.post('/post', authentication);
 app.post('/post', routes.Post.doPost);
-app.get('/edit/:id',authentication);
-app.get('/edit/:id',routes.Post.edit);
-app.post('/edit/:id',authentication);
-app.post('/edit/:id',routes.Post.doEdit);
-app.get('/remove/:id',authentication);
-app.get('/remove/:id',routes.Post.doRemove);
+app.get('/edit/:id', authentication);
+app.get('/edit/:id', routes.Post.edit);
+app.post('/edit/:id', authentication);
+app.post('/edit/:id', routes.Post.doEdit);
+app.get('/remove/:id', authentication);
+app.get('/remove/:id', routes.Post.doRemove);
 
 var server = http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
-
-
--- routes/project.js
+++ routes/project.js
@@ -1,11 +1,15 @@
-exports.doing = function(req,res){
-	return res.render('doing');
+exports.doing = function(req, res) {
+    return res.render('doing');
 }
 
-exports.dontTouchWhite = function(req,res){
-	return res.render('project/dontTouchWhite');
+exports.dontTouchWhite = function(req, res) {
+    return res.render('project/dontTouchWhite');
 }
 
-exports.articlesClassify = function(req,res){
-	return res.render('project/articlesClassify');
+exports.articlesClassify = function(req, res) {
+    return res.render('project/articlesClassify');
+}
+
+exports.PsychologicalExperiment = function(req, res) {
+    return res.render('project/PsychologicalExperiment');
 }
