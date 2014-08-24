ab0930365d2c6fbaf498cd8a3fce878e1b8accde
刘春洋 <lcya86@qq.com>
2014-08-24 17:28:34 +0800

    trim

--- models/psycological/material.js
+++ models/psycological/material.js
@@ -3,14 +3,14 @@ var Schema = mongoose.Schema;
 
 var materialImgSchema = new Schema({
 	content: String,
-	sequence:int,//序号相同为一对
-	polarity: int//0为消极，1为积极
+	sequence: Number,//序号相同为一对
+	polarity: Number//0为消极，1为积极
 });
 mongoose.model('MaterialImg', materialImgSchema);
 
 var materialTextSchema = new Schema({
-	sententce:String,
-	pword:String,
-	nword:String
+	sententce: String,
+	pword: String,
+	nword: String
 });
 mongoose.model('MaterialText',materialTextSchema);
