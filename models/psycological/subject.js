08298c72c67b9f1010dc1e0b459520a730551b93
刘春洋 <lcya86@qq.com>
2014-10-05 21:31:15 +0800

    tr9iim

--- models/psycological/subject.js
+++ models/psycological/subject.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema;
 var subjectSchema = new Schema({
 	name: String,
 	complete:[Boolean],
-	type: Number//1为实验组1,2为实验组2,0为控制组
+	type: Number//1为实验组1,2为实验组2,0为控制组1,-1为控制组2
 })
 mongoose.model('Subject', subjectSchema);
