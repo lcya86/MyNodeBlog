9fc646960047dc35a5fed35419c9cfd621ce10ea
刘春洋 <lcya86@qq.com>
2015-01-31 18:56:00 +0800

    trimm

--- routes/project.js
+++ routes/project.js
@@ -56,6 +56,7 @@ exports.saveImage = function(req,res){
   var timestamp = req.param("timestamp");
   var parent = req.body.parent || 0;
   var base64 = req.body.base64;
+  console.log(base64);
   model.Images.create({
     timestamp:timestamp,
     parent: parent,
