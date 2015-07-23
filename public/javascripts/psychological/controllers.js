0a23038b3d174577f54c12c2e34cdeb727bab560
刘春洋 <lcya86@qq.com>
2015-07-23 22:41:38 +0800

    trim

--- public/javascripts/psychological/controllers.js
+++ public/javascripts/psychological/controllers.js
@@ -53,6 +53,10 @@ angular.module('psychological',['psychological.directives'])
       alert('请添加图片');
       return false;
     }
+    if($scope.letter===''||$scope.position==0){
+      alert('请填写显示字母和积极概率')；
+      return false;
+    }
     $http.post('/project/psychological/v2/addpairs',{
       upImg: $scope.upImg,
       downImg: $scope.downImg,
