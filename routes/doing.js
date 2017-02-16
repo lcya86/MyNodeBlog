cc328d141a48308f08a3b1ec9f1279e9915d919b
刘春洋 <lcya86@qq.com>
2017-02-16 21:14:24 +0800

    modify test

--- routes/doing.js
+++ routes/doing.js
@@ -5,7 +5,7 @@ exports.doing = function(req,res){
 
 exports.getResponse = function(req,res){
   console.log(req);
-  cnosole.log(req.session.user);
+  console.log(req.session.user);
   res.set('Access-Control-Allow-Origin','*');
   res.cookie('name', 'tobi', { domain: 'lcy-blog.com', path: '/test', secure: true });
   return res.send({data:'aaa'});
